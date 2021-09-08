cc600a060d0d34697cd5f0cfdbb2319a1fec8cfc
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2021-09-08 22:13:57 +0800

    not use eslint vue project

---
+++ zh-copy/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <column-list :list='list'></column-list>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import columnList, { columnProps } from './components/columnList.vue'
+
+const testData: columnProps[] = [
+  {
+    id: 1,
+    title: 'test1',
+    description: '这是test专栏，有一段非常有意思的简介，可以更新一下',
+    avatar: 'https://images.unsplash.com/photo-1593642532744-d377ab507dc8?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80'
+  },
+  {
+    id: 2,
+    title: 'test1',
+    description: '这是test专栏，有一段非常有意思的简介，可以更新一下',
+    avatar: 'https://images.unsplash.com/photo-1593642532744-d377ab507dc8?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80'
+  }
+]
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    columnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ zh-copy/src/components/columnList.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <ul>
+        <li v-for="i in list" :key="i.id">
+            <img :src="i.avatar" :alt="i.title">
+            <h5>{{i.title}}</h5>
+            <p>{{i.description}}</p>
+            <a href="#">进入专栏</a>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface columnProps {
+    id:number;
+    title:string;
+    avatar:string;
+    description:string;
+}
+
+export default defineComponent({
+  name: 'columnList',
+  props: {
+    list: {
+      type: Array as PropType<columnProps[]>,
+      required: true
+    }
+  },
+  setup () {
+
+  }
+})
+</script>
