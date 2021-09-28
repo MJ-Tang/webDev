db6b40f34d81838e0023a0bba9aa8cdb1547a79c
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2021-09-28 23:02:59 +0800

    add axios

--- zhcopy/src/main.ts
+++ zhcopy/src/main.ts
@@ -1,13 +1,21 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
 
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 
-
-
+axios.defaults.baseURL = 'https://apis.imooc.com/api/'
+axios.interceptors.request.use(config => {
+    config.params = {
+        ...config.params, icode: '9BFBE31E842410AF'
+    }
+    return config
+})
+axios.get('columns').then(resp => {
+    console.log(resp.data);
+})
 const app = createApp(App)
 app.use(router)
 app.use(store)
